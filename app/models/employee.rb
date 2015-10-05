809c18db26f44d53f65d16e60a96a65e62c518c8
sanjuvds <sanjuvds@gmail.com>
2015-10-06 00:54:08 +0530

    Added the sign up form validation

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,6 +1,18 @@
 class Employee < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  
   has_many :timesheets
+
+  validates :forname, :format => { :with => /\A[a-zA-Z0-9\ \'\-]+$\z/, :message => "only letters allowed" },
+                :length => { :maximum => 30 },
+                :if => Proc.new {|employee| employee.forname.present? }
+  validates :surname,
+            :format => { :with => /\A[a-zA-Z0-9\ \'\-]+$\z/, :message => "only letters allowed" },
+            :length => { :maximum => 30 },
+            :if => Proc.new {|employee| employee.surname.present? }
+
+  validates :phone, :length => {:minimum => 7, :maximum => 20},
+                :allow_blank => true,
+                :format => {:with => /\A[\s0-9#()+-\[\]\|:;',.?\/\\]*$\z/}
+
 end
