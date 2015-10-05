b5678b417e0c7e780759e44d58d4064fb1e4cffc
sanjuvds <sanjuvds@gmail.com>
2015-10-06 02:02:42 +0530

    updated some part of code

--- app/controllers/managers_controller.rb
+++ app/controllers/managers_controller.rb
@@ -17,14 +17,7 @@ class ManagersController < ApplicationController
 
   # GET /managers/1
   # GET /managers/1.json
-  def show
-  end
-
-  # GET /managers/new
-  def new
-    @manager = Manager.new
-  end
-
+  
   def popup_declined
     @timesheet = Timesheet.find_by(id: params[:manager_id])
     respond_to do |format|
@@ -32,26 +25,6 @@ class ManagersController < ApplicationController
     end
   end
 
-  # GET /managers/1/edit
-  def edit
-  end
-
-  # POST /managers
-  # POST /managers.json
-  def create
-    @manager = Manager.new(manager_params)
-
-    respond_to do |format|
-      if @manager.save
-        format.html { redirect_to @manager, notice: 'Manager was successfully created.' }
-        format.json { render :show, status: :created, location: @manager }
-      else
-        format.html { render :new }
-        format.json { render json: @manager.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-  
   def reject
     timesheet = params[:timesheet]
     timesheet = Timesheet.find_by(timesheet_id)
@@ -62,27 +35,7 @@ class ManagersController < ApplicationController
       format.json { head :no_content }
     end
   end  
-
-  def update
-    respond_to do |format|
-      if @manager.update(manager_params)
-        format.html { redirect_to @manager, notice: 'Manager was successfully updated.' }
-        format.json { render :show, status: :ok, location: @manager }
-      else
-        format.html { render :edit }
-        format.json { render json: @manager.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def destroy
-    @manager.destroy
-    respond_to do |format|
-      format.html { redirect_to managers_url, notice: 'Manager was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
+ 
   private
     def set_manager
       @manager = Manager.find(params[:id])
